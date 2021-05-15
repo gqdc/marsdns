1b0d40bb0adf4d6a89d91d1209018cba3c813efc
Olivier M <42801943+gqdc@users.noreply.github.com>
2021-05-15 16:00:45 +0200

    Update marsdns.sh
    Typos

--- marsdns.sh
+++ marsdns.sh
@@ -8,7 +8,7 @@
 
 # Please define these settings
 SERVER="" # https://url_to_haproxy_update_script/
-USER="" # a usern allowed to make basic apache auth on SERVER
+USER="" # a user allowed to make basic apache auth on SERVER
 PASSWORD="" # The user's password
 NAME="maison" # An arbitrary string to authentify the user agent of the curl command. Used on the server side.
 
