642283464a91d392c80a739ef9ed9118909a76c7
Olivier M <42801943+gqdc@users.noreply.github.com>
2021-05-15 16:07:59 +0200

    Update marsdns.sh
    Comments improvment

--- marsdns.sh
+++ marsdns.sh
@@ -17,9 +17,9 @@ PASSWORD=""
 NAME=""
 
 # Optional settings (default are good)
-# Where to write the log file
+# Where to write the log file. Default is /var/log/marsdns.log
 LOGFILE="/var/log/${0%%.*}.log"
-# Date format. Default correspon to : 2021-05-15T15:54:47+02:00
+# Date format. Default correspond to : 2021-05-15T15:54:47+02:00
 DATE=`date --iso-8601=seconds`
 
 [ ! -f "$LOGFILE" ] && touch "$LOGFILE"
