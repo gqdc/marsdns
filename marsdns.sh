f08d2965916b7ee3675b5827f8e36cc5f56a57a3
Olivier M <42801943+gqdc@users.noreply.github.com>
2021-05-15 16:07:12 +0200

    Update marsdns.sh
    Comments improvment.

--- marsdns.sh
+++ marsdns.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-# This script make a curl request to a proxy server.
-# It is used to make a fake dynDNS.
+# This script make a curl request to server that will update
+# the IP of a server within a HAProxy backedn.
 #
-# You must define SERVER, wich is the proxy server
-# and add a task in the crontab to run the script
-# every 5 minutes.
+# Don't forget to add this script in a crontab, like : 
+# */5 * * * * bash /usr/local/share/marsdns.sh
+#
+# git : https://github.com/gqdc/marsdns
 
 # Please define these settings
-SERVER="" # https://url_to_haproxy_update_script/
-USER="" # a user allowed to make basic apache auth on SERVER
-PASSWORD="" # The user's password
-NAME="maison" # An arbitrary string to authentify the user agent of the curl command. Used on the server side.
+# URL to the haproxy update sscript, i.e : https://url_to_haproxy_update_script/
+SERVER=""
+# User credentials to make basic apache auth on SERVER.
+USER=""
+PASSWORD=""
+# An arbitrary string to authentify the user agent at server side.
+NAME=""
 
 # Optional settings (default are good)
+# Where to write the log file
 LOGFILE="/var/log/${0%%.*}.log"
+# Date format. Default correspon to : 2021-05-15T15:54:47+02:00
 DATE=`date --iso-8601=seconds`
 
 [ ! -f "$LOGFILE" ] && touch "$LOGFILE"
 
-echo ${DATE} `curl -fs -AdynDns-${NAME} -u${USER}:${PASSWORD} --url ${SERVER}` >> ${LOGFILE}
+echo ${DATE} - `curl -fs -AdynDns-${NAME} -u${USER}:${PASSWORD} --url ${SERVER}` >> ${LOGFILE}
